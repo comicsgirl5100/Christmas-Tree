b81717ecc3702720912190557a6c66f2b75adbaf
TheJakester42 <jacob.moulton@gmail.com>
2017-12-02 01:08:35 -0700

    better user input
    added infinite loop for user input

--- square.py
+++ square.py
@@ -1,37 +1,40 @@
+#imports
 import math
 
-print("enter tree height: ")
-treeHeight = int(input())
 
-print("enter tree width: ")
-treeWidth = int(input())
+while(True):
+    #ask user for info
+    print("enter tree height: ")
+    treeHeight = int(input())
 
-print("Enter stump width")
-stumpWidth = int(input())
+    print("enter tree width: ")
+    treeWidth = int(input())
 
-if stumpWidth >= treeWidth:
-    print("Please choose a stump width smaller than the tree width")
-    stumpWidth = int(input())
-
-else:
-    print("Enter stump height")
+    print("Enter stump height: ")
     stumpHeight = int(input())
 
-    if stumpHeight >= treeHeight:
-        print("Please choose a stump height smaller than the tree height")
-        stumpHeight = int(input())
+    print("Enter stump width: ")
+    stumpWidth = int(input())
+
+    if(treeHeight < stumpHeight or treeWidth < stumpWidth):
+        #ask user for info again
+        print("bad input, try again")
+    else:
+        #good info, carry on
+        break
+print("1", end="")
+print("2", end="")
+print("3")
+
+for i in range(treeHeight):
+    print (" "*(treeWidth))*(treeHeight-i-1) + (("*"*(int(math.floor(treeWidth/2)))+"*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
+
+
+stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
+
+for i in range(stumpHeight):
+    if i in[0]:
+        print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
 
     else:
-
-        for i in range(treeHeight):
-            print (" "*(treeWidth))*(treeHeight-i-1) + (("*"*(int(math.floor(treeWidth/2)))+ "*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
-    
-
-        stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
-
-        for i in range(stumpHeight):
-            if i in[0]:
-                print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
-
-            else:
-                print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+        print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
