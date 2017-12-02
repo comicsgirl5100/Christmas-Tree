befa2b2a2bef0578ef3a1dd120fe5e99310c1973
Halie <haliehagerty@gmail.com>
2017-12-02 00:51:33 -0700

    Stump Size Limiter
    Constrains the stump size based on the tree size. (Can't get the code to continue in if portion)

--- square.py
+++ square.py
@@ -9,21 +9,29 @@ treeWidth = int(input())
 print("Enter stump width")
 stumpWidth = int(input())
 
-print("Enter stump height")
-stumpHeight = int(input())
+if stumpWidth >= treeWidth:
+    print("Please choose a stump width smaller than the tree width")
+    stumpWidth = int(input())
 
+else:
+    print("Enter stump height")
+    stumpHeight = int(input())
 
-
-
-for i in range(treeHeight):
-    print (" "*(treeWidth))*(treeHeight-i-1) + (("*"*(int(math.floor(treeWidth/2)))+ "*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
-    
-
-stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
-
-for i in range(stumpHeight):
-    if i in[0]:
-        print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+    if stumpHeight >= treeHeight:
+        print("Please choose a stump height smaller than the tree height")
+        stumpHeight = int(input())
 
     else:
-        print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+
+        for i in range(treeHeight):
+            print (" "*(treeWidth))*(treeHeight-i-1) + (("*"*(int(math.floor(treeWidth/2)))+ "*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
+    
+
+        stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
+
+        for i in range(stumpHeight):
+            if i in[0]:
+                print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+
+            else:
+                print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
