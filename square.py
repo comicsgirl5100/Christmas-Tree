af6f6a91b16d9f77b7b394e4a28c3506ede85c1e
Halie <haliehagerty@gmail.com>
2017-12-04 22:35:14 -0700

    Print on same line
    I made it so that the Spaces print on the same line.  it makes it so that the code a little easier to read/not in all the same line.

--- square.py
+++ square.py
@@ -1,6 +1,16 @@
+
 #imports
+from __future__ import print_function
 import math
 
+def printf(str, *args):
+    print(str % args, end='')
+
+
+for x in xrange(10):
+    printf('.')
+print("done")
+#..........done
 
 while(True):
     #ask user for info
@@ -22,19 +32,29 @@ while(True):
     else:
         #good info, carry on
         break
+    
 print("1", end="")
 print("2", end="")
 print("3")
-
+print(" ", end="")
+      
 for i in range(treeHeight):
-    print (" "*(treeWidth))*(treeHeight-i-1) + (("*"*(int(math.floor(treeWidth/2)))+"*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
+    for x in xrange((treeWidth)*(treeHeight-i-1)):
+        printf(" ") #, (end=""))*((treeWidth))*(treeHeight-i-1))
+
+    print(("*"*(int(math.floor(treeWidth/2)))+"*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
 
 
 stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
 
 for i in range(stumpHeight):
     if i in[0]:
-        print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+        for x in xrange(stumpTab+1):
+            printf(" ")
+        print("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2))))
 
     else:
-        print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+        for x in xrange(stumpTab+1):
+            printf(" ")
+        print("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2))))
+        #print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
