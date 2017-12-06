b16e5ac37aac0dce8f9525ad56fee6a92dce6b26
Halie <haliehagerty@gmail.com>
2017-12-06 12:12:29 -0700

    Working Christmas Tree Top
    The christmas tree now prints all of the astrics correctly and centered. (plus I wrote all the code myself!)

--- square.py
+++ square.py
@@ -1,60 +1,92 @@
-
 #imports
 from __future__ import print_function
 import math
 
-def printf(str, *args):
-    print(str % args, end='')
-
-
-for x in xrange(10):
-    printf('.')
-print("done")
-#..........done
-
+#dimensions for tree and stump
 while(True):
     #ask user for info
-    print("enter tree height: ")
-    treeHeight = int(input())
+    print("Enter tree width: ")
+    treeWidth = float(input())
 
-    print("enter tree width: ")
-    treeWidth = int(input())
+    print("Enter tree height (Must be at least half the width):")
+    treeHeight = float(input())
 
     print("Enter stump height: ")
-    stumpHeight = int(input())
+    stumpHeight = float(input())
 
     print("Enter stump width: ")
-    stumpWidth = int(input())
+    stumpWidth = float(input())
 
-    if(treeHeight < stumpHeight or treeWidth < stumpWidth):
+    if(
+        treeHeight > stumpHeight and
+        treeWidth > stumpWidth and
+        treeHeight >= (treeWidth/2)
+        ):
         #ask user for info again
-        print("bad input, try again")
+        print("Good input! I'll draw a tree now")
+        break
+        
     else:
         #good info, carry on
-        break
-    
-print("1", end="")
-print("2", end="")
-print("3")
-print(" ", end="")
-      
-for i in range(treeHeight):
-    for x in xrange((treeWidth)*(treeHeight-i-1)):
-        printf(" ") #, (end=""))*((treeWidth))*(treeHeight-i-1))
-
-    print(("*"*(int(math.floor(treeWidth/2)))+"*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
+        print("Bad input, try again")
 
 
-stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
+def astrics(treeWidth):
+    for i in range(treeWidth):
+        print("*", end='')
+    print()
+        
+#to make center line
+if(treeWidth%2 == 1):
+    pass
+else:
+    treeWidth += 1
 
-for i in range(stumpHeight):
-    if i in[0]:
-        for x in xrange(stumpTab+1):
-            printf(" ")
-        print("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2))))
+for i in range(1,int(treeHeight-1)):
+    print(i)
 
+print(">>>>>>>>>>")
+
+#top of tree
+for i in range(int(treeWidth/2)):
+    print(" ", end='')
+print("*")
+#values for middle of tree
+treeRatio = (math.ceil((treeHeight)/(treeWidth)))
+treeRatio = int(treeRatio)
+
+#middle of tree
+for i in range(1,int(treeHeight-1)):
+    #this determines the number of astrics that the tree row needs
+    numAstrics = ((i+1)*(treeRatio))
+    numAstrics = int(numAstrics)
+    #this maked it so the tree lines up in the middle
+    if (numAstrics%2 == 1):
+        pass
     else:
-        for x in xrange(stumpTab+1):
-            printf(" ")
-        print("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2))))
-        #print((" "*stumpTab) + ("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2)))))
+        numAstrics += 1
+    #this determines the spaces that the row needs to line up
+    spaces = (((int(treeWidth)) - numAstrics)/2)
+    for i in range(spaces):
+        print(" ", end='')
+    print("*"*numAstrics)
+
+#bottom of tree
+astrics(int(treeWidth))
+
+###code that currently works to print tree but not correct width.
+###prints tree part     
+##for i in range(treeHeight):
+##    for x in range((treeWidth)*(treeHeight-i-1)):
+##        print(" ", end='')
+##
+##    print(("*"*(int(math.floor(treeWidth/2))) + "*" + "*"*(int(math.floor(treeWidth/2))))*(2*i+1))
+##
+##
+##stumpTab = ((treeWidth)*(treeHeight-1) + (int(math.floor(treeWidth/2))) - (int(math.floor(stumpWidth/2))))
+##
+###prints stump`
+##for i in range(stumpHeight):
+##    for x in range(stumpTab):
+##        print(" ", end='')
+##    print("*"*(int(math.floor(stumpWidth/2))) + "*" + "*"*(int(math.floor(stumpWidth/2))))
